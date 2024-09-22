29de834eafe603969ec4ef28752d966e794aecd2
IT <IT@MASTER>
2024-09-22 20:30:35 +0300

    Add project files.

---
+++ 4islemhesplayan_geriye_deger_donduren_metot/Program.cs
@@ -0,0 +1,19 @@
+﻿using _4islemhesplayan_geriye_deger_donduren_metot;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        dort_islem islem = new dort_islem();
+        Console.WriteLine("1. Sayıyı giriniz: ");
+        int s1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("2. Sayıyı giriniz: ");
+        int s2 = int.Parse(Console.ReadLine());
+        islem.topla(s1, s2);
+        islem.cikar(s1, s2);
+        islem.bol(s2, s1);
+        islem.carp(s1, s2);
+
+
+    }
+}
---
+++ 4islemhesplayan_geriye_deger_donduren_metot/dort_islem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4islemhesplayan_geriye_deger_donduren_metot
+{
+    internal class dort_islem
+    {
+        public int topla(int sayi1, int sayi2)
+        {
+            int sonuc = sayi1 + sayi2;
+            Console.WriteLine("Toplam :"+sonuc);
+            return sonuc;
+        }
+
+        public int carp(int sayi1, int sayi2)
+        {
+            int carp = sayi1*sayi2;
+            Console.WriteLine("Çarpım :" + carp);
+            return carp;
+        }
+        public double bol(double sayi1, double sayi2)
+        {
+            double bol = sayi1/sayi2;
+            Console.WriteLine("Bölüm :"+bol);
+            return bol;
+        }
+
+        public int cikar(int sayi1, int sayi2) { 
+            int cikar= sayi1-sayi2;
+            Console.WriteLine("Çıkar :"+cikar);
+            return cikar;
+        }
+    }
+}
